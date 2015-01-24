2ee19f9b03285143795bfa881166a22e7e6952c7
James Reilly <reillyj33@gmail.com>
2015-01-24 16:34:49 +1100

    Site updated at 2015-01-24 05:34:49 UTC

---
+++ javascripts/open-in-blank.js
@@ -0,0 +1,10 @@
+$(document).bind('DOMNodeInserted', function(event) {
+        $('a[href^="http"]').each(
+            function(){
+                if (!$(this).attr('target')) {
+                    $(this).attr('target', '_blank')
+                }
+
+            }
+        );
+});
