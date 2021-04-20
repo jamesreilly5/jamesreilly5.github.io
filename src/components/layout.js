4dd279875f36041c8235d1768182d213741977a3
James Reilly <reillyj33@gmail.com>
2021-04-20 20:29:23 +1000

    Add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        Curious about what I'm building? Take a look at 
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="http://usertake.com/">usertake.com</a>
       </footer>
     </div>
   )
